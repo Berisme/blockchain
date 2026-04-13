2834a0938013de6580355e811d365f1beb7bb5b4
Berisme <nhp.ber@gmail.com>
2026-04-13 09:42:21 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,34 +3,29 @@ const path = require("path");
 
 const app = express();
 
-// middleware
 app.use(express.json());
 
-// cho phép load file HTML
+// cho phép load file tĩnh
 app.use(express.static(__dirname));
 
-// dữ liệu giả (demo)
-let products = [];
-
-// 👉 TRANG CHỦ (HIỂN THỊ index.html)
+// route trang chủ
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// 👉 THÊM SẢN PHẨM
+// data fake
+let products = [];
+
+// thêm
 app.post("/add", (req, res) => {
   const { name, origin } = req.body;
 
-  products.push({
-    name,
-    origin,
-    time: Date.now()
-  });
+  products.push({ name, origin, time: Date.now() });
 
   res.send("Them san pham thanh cong!");
 });
 
-// 👉 LẤY SẢN PHẨM
+// lấy
 app.get("/product/:id", (req, res) => {
   const p = products[req.params.id];
 
@@ -39,6 +34,5 @@ app.get("/product/:id", (req, res) => {
   res.json(p);
 });
 
-// chạy server (QUAN TRỌNG cho Render)
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server running"));
